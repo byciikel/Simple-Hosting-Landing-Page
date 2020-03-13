c18c5e13622d8f00716ff12d213d8a5fdf43090f
Bayu Utomo <by.ciikel@gmail.com>
2020-03-14 01:33:27 +0700

    chore: add hamburger menu for mobile screen

--- bayu_utomo-fe-2020.js
+++ bayu_utomo-fe-2020.js
@@ -6,4 +6,31 @@ $(document).ready(function(){
     animateTime: 0.7,
     animateFunction: 'ease'
   });
+
+  function toggleSidebar(e) {
+    $(".hamburger-button").toggleClass("active");
+
+    let isActive = e.currentTarget.className.split(' ')[1] ? true : false
+
+    if (isActive) {
+      $(".sidebar").removeClass("move-to-right");
+      $(".sidebar").toggleClass("move-to-left");
+    } else {
+      $(".sidebar").removeClass("move-to-left");
+      $(".sidebar").toggleClass("move-to-right");
+    }
+    console.log(isActive)
+
+    $(".sidebar-item").toggleClass("active");
+  }
+
+  $(".hamburger-button").on("click tap", function(e) {
+    toggleSidebar(e);
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      toggleSidebar();
+    }
+  });
 });
